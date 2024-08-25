9e4acc5e0ce211f080f8ae57ed6941a6b56b8d74
krebs-et <chava.r.lol@gmail.com>
2024-08-25 16:24:19 -0600

    "Se retira menú desplegable y se comenta archivo JS"

--- main.js
+++ main.js
@@ -6,22 +6,22 @@ navToggle.addEventListener('click', () => {
     navMenu.classList.toggle("nav_menu_visible");
 })
 
-const COLOR_DEFAULT = '#f7f6f6'
-const VISIBILITY_DEFAULT = 'none'
+// const COLOR_DEFAULT = '#f7f6f6'
+// const VISIBILITY_DEFAULT = 'none'
 
-function changeColor (elemento, color, tarjeta, visibilidad) {
-    changeTodosColor();
-    elemento.style.backgroundColor = color;
-    tarjeta.style.display = visibilidad;
-}
+// function changeColor (elemento, color, tarjeta, visibilidad) {
+//     changeTodosColor();
+//     elemento.style.backgroundColor = color;
+//     tarjeta.style.display = visibilidad;
+// }
 
-function changeTodosColor(){
-    document.querySelectorAll('.catalog_menu > li').forEach(e => {
-        e.style.backgroundColor = COLOR_DEFAULT;
-    });
-    document.querySelectorAll('.area_wellness_cards > .area_wellness_card').forEach(i => {
-        i.style.display = VISIBILITY_DEFAULT;
-    });
-}
+// function changeTodosColor(){
+//     document.querySelectorAll('.catalog_menu > li').forEach(e => {
+//         e.style.backgroundColor = COLOR_DEFAULT;
+//     });
+//     document.querySelectorAll('.area_wellness_cards > .area_wellness_card').forEach(i => {
+//         i.style.display = VISIBILITY_DEFAULT;
+//     });
+// }
 
 
